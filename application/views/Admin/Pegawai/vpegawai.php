ddf3f4da98589bdb4bac040cdb02582fd94c09f8
wasilaturohim <wasilaturohim@student.telkomuniversity.ac.id>
2024-11-04 14:52:22 +0700

    edit: vpegawai

--- application/views/Admin/Pegawai/vpegawai.php
+++ application/views/Admin/Pegawai/vpegawai.php
@@ -30,7 +30,7 @@
         } ?>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-9">
+                <div class="col-12">
                     <div class="card">
                         <div class="card-header">
                             <h3 class="card-title">Informasi Data Pegawai</h3>
